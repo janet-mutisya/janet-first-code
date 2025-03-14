4240002bc5ce6fb4c009924652597c09300e26eb
janet-mutisya <janetymutisya@gmail.com>
2025-03-14 09:21:48 +0300

    arithimetics

---
+++ janet.py
@@ -0,0 +1,11 @@
+a=5
+b=14
+    #addition
+print('sum: ', a + b)
+    #subtraction
+print("subtraction: ", a - b)
+    #multiplication
+print("multiplication: ", a * b)
+     #division
+print("division: ", a / b)
+      
